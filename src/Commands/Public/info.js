96658a2f7212e5ac76853cbd9eeaa79a53163674
Huguitis <65133800+Huguitis@users.noreply.github.com>
2022-01-20 21:39:17 +0100

    Update info.js

--- src/Commands/Public/info.js
+++ src/Commands/Public/info.js
@@ -4,29 +4,29 @@ aliases: ["help", "ayuda"],
 code: `
 $description[1;
 **__$username[$findUser[$message]]'s Account:__**
-• $username[$findUser[$message]] have sent **$getGlobalUserVar[messagecount;$findUser[$message]]** messages to Huguitis's Alexa!
+• $username[$findUser[$message]] have sent **$getGlobalUserVar[messagecount;$findUser[$message]]** messages to <@$getVar[OwnerID]>'s Alexa!
 • ¿Is $username[$findUser[$message]] banned? **$getGlobalUserVar[blacklist]**
 
 **__Bot Commands:__**
 • **Public:**
-- \`alexa info\` Shows this help message.
-- \`alexa send\` Send a message to <@$getVar[OwnerID]>'s Alexa!
+- \`$getVar[DiscordBotPrefix]info\` Shows this help message.
+- \`$getVar[DiscordBotPrefix]send\` Send a message to <@$getVar[OwnerID]>'s Alexa!
 
 • **Private:**
-- \`alexa eval\` Execute a code with Discord.
-- \`alexa djseval\` Execute a js code with Discord.
-- \`alexa blacklist\` Blacklist an user in the bot.
-- \`alexa unblacklist\` Unblacklist an user in the bot.
-- \`alexa activate\` Allow users to send messages.
-- \`alexa desactivate\` Block users to send messages.
+- \`$getVar[DiscordBotPrefix]eval\` Execute a code with Discord.
+- \`$getVar[DiscordBotPrefix]djseval\` Execute a js code with Discord.
+- \`$getVar[DiscordBotPrefix]blacklist\` Blacklist an user in the bot.
+- \`$getVar[DiscordBotPrefix]unblacklist\` Unblacklist an user in the bot.
+- \`$getVar[DiscordBotPrefix]activate\` Allow users to send messages.
+- \`$getVar[DiscordBotPrefix]desactivate\` Block users to send messages.
 
 **• [Bot Invite]($getBotInvite[admin])**
-**• [Support Server]($getVar[MainServerInvite])**
+**• [Support Server](https://discord.gg/et2BERsYSM)**
 ]
 
 $color[1;GREEN]
 
 $thumbnail[1;$userAvatar[$findUser[$message]]]
 
-$footer[1;Created by $userTag[$getVar[OwnerID]]]`
+$footer[1;Created by ! Huguitis#4927]`
 })
