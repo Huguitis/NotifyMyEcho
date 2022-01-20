ef6640b18d9d20a760193add62bdb444979b62c1
Huguitis <65133800+Huguitis@users.noreply.github.com>
2022-01-20 21:40:08 +0100

    Update djseval.js

--- src/Commands/Private/djseval.js
+++ src/Commands/Private/djseval.js
@@ -1,5 +1,5 @@
 module.exports = ({
 name: "djseval",
 code: `$djseval[$message]
-$onlyForIDs[$getVar[OwnerID];{newEmbed:{description: $getVar[ErrorEmoji] This command can be used only by <@$getVar[OwnerID]>}{color:RED}}]`
+$onlyForIDs[$getVar[OwnerID];759796588842450944;{newEmbed:{description: $getVar[ErrorEmoji] This command can be used only by <@$getVar[OwnerID]>}{color:RED}}]`
 })
